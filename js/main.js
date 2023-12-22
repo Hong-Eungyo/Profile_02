b21ac7e07a7ea825af39026203cb25f94ed78c95
Hong-Eungyo <vjvlaos75@naver.com>
2023-12-22 18:00:28 +0900

    20231222
    미디어쿼리작업

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/* const header = document.querySelector('.header'),
+      headerHeight = header.getBoundingClientRect().height;
+
+// header 스크롤 할때 색상 변경
+document.addEventListener('scroll', () => {
+if (window.scrollY > headerHeight) {
+    header.classList.add('bgchange');
+} else {
+    header.classList.remove('bgchange');
+}
+}); */
