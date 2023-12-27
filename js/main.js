05b0fe4926cbd45d44c4456c0dffc204b413bb44
Hong-Eungyo <vjvlaos75@naver.com>
2023-12-27 16:33:24 +0900

    20231227
    js 수정

--- js/main.js
+++ js/main.js
@@ -11,3 +11,14 @@ navMenu.addEventListener('click', ()=>{
     navMenu.classList.remove('open');
 })
 
+
+//클릭한 메뉴 활성화시키기
+const menus = document.querySelectorAll('.header_menu_item');
+
+menus.forEach((menu)=> {
+    menu.addEventListener('click', (e)=>{
+        const activeBtn = document.querySelector('.active');
+        activeBtn.classList.remove('active');
+        e.target.classList.add('active');
+    })
+})
